75b2c2abe1a40685150738284cb9dbd186a43d87
Katy Gorman <katygorman7@gmail.com>
2020-10-22 15:52:08 +0100

    Updated so that ids are replaced by names

--- Effective Profit or Loss.sql
+++ Effective Profit or Loss.sql
@@ -1,14 +1,14 @@
-SELECT 	buys.deal_counterparty_id, 
-		buys.deal_instrument_id, 
+SELECT 	counterparty_name, 
+		instrument_name, 
 		((sell_amount - buy_amount) + ((buy_quantity - sell_quantity) * current_market_value)) AS effective_profit
 FROM (
-	SELECT deal_counterparty_id, deal_instrument_id, sum(deal_amount*deal_quantity) AS buy_amount, sum(deal_quantity) as buy_quantity
+	SELECT deal_counterparty_id, deal_instrument_id, SUM(deal_amount*deal_quantity) AS buy_amount, SUM(deal_quantity) AS buy_quantity
 	FROM deal
 	WHERE deal_type = 'B'
 	GROUP BY deal_instrument_id, deal_counterparty_id
 ) buys
 JOIN (
-	SELECT deal_counterparty_id, deal_instrument_id, sum(deal_amount*deal_quantity) AS sell_amount, sum(deal_quantity) as sell_quantity
+	SELECT deal_counterparty_id, deal_instrument_id, SUM(deal_amount*deal_quantity) AS sell_amount, SUM(deal_quantity) AS sell_quantity
 	FROM deal
 	WHERE deal_type = 'S'
 	GROUP BY deal_instrument_id, deal_counterparty_id
@@ -23,4 +23,14 @@ JOIN (
 		GROUP BY deal_instrument_id)
 ) market_value
 ON buys.deal_instrument_id = market_value.deal_instrument_id
-ORDER BY buys.deal_counterparty_id, buys.deal_instrument_id;
+JOIN (
+	SELECT * 
+    FROM instrument
+) instruments
+ON buys.deal_instrument_id = instruments.instrument_id
+JOIN (
+	SELECT counterparty_id, counterparty_name
+    FROM counterparty
+) counterparty
+ON buys.deal_counterparty_id = counterparty.counterparty_id
+ORDER BY counterparty_name, instrument_name;
